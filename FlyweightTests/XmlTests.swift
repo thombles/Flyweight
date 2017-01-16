d843d5d3b130f3ddd460a121d8a81f504620e828
Thomas Karpiniec <tk@1.21jiggawatts.net>
2017-01-16 22:09:28 +1100

    Partial unit testing of post XML parsing

--- FlyweightTests/XmlTests.swift
+++ FlyweightTests/XmlTests.swift
@@ -34,22 +34,47 @@ class XmlTests: XCTestCase {
         return try! Data(contentsOf: url!)
     }
     
-    func testImageAttachment() {
-        let data = loadDataFile(filename: "ImageNoticeFeed", ext: "xml")
+    // Generic wrapper for testing the parsing of XML feeds that should succeed
+    private func tryParseXml(filename: String, tests: @escaping ([ASEntry]) -> ()) {
+        let data = loadDataFile(filename: filename, ext: "xml")
         let exp = expectation(description: "Parsing complete")
         let parser = FeedParser()
         parser.parseEntries(data: data) { entries, error in
-            let ent = entries!
-            XCTAssertEqual(ent.count, 1)
-            let entry = ent[0]
+            XCTAssert(entries != nil, "Entries should not be nil")
+            tests(entries!)
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+    
+    func testBasicPost() {
+        tryParseXml(filename: "PostNoticeFeed") { entries in
+            XCTAssertEqual(entries.count, 1)
+            let entry = entries[0]
+            XCTAssertTrue(entry.isPost)
+            XCTAssertFalse(entry.isReply)
+            XCTAssertFalse(entry.isRepeat)
+            XCTAssertFalse(entry.isFavourite)
+            XCTAssertFalse(entry.isComment)
+            XCTAssertFalse(entry.isDelete)
+            XCTAssertEqual(entry.objectType, "http://activitystrea.ms/schema/1.0/note")
+            XCTAssertEqual(entry.id, "tag:gs1.karp.id.au,2017-01-15:noticeId=13:objectType=note")
+            XCTAssertEqual(entry.htmlLink, "https://gs1.karp.id.au/notice/13")
+            XCTAssertEqual(entry.statusNetNoticeId, 13)
+            XCTAssertEqual(entry.activityVerb, "http://activitystrea.ms/schema/1.0/post")
+        }
+    }
+    
+    func testImageAttachment() {
+        tryParseXml(filename: "ImageNoticeFeed") { entries in
+            XCTAssertEqual(entries.count, 1)
+            let entry = entries[0]
             XCTAssertEqual(entry.enclosures.count, 1)
             let enclosure = entry.enclosures[0]
             XCTAssertEqual(enclosure.url, "https://gs1.karp.id.au/file/cc38cb87dc2b955f3b7b6630b2d6e6506df65b4166fbabe8d5a08b27678d4b1c.png")
             XCTAssertEqual(enclosure.mimeType, "image/png")
             XCTAssertEqual(enclosure.length, 32470)
-            exp.fulfill()
         }
-        waitForExpectations(timeout: 5.0, handler: nil)
     }
     
 }
