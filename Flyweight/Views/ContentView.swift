43e53c65480fb2b1771be6c9b8c315d1d4f434e6
Thomas Karpiniec <tk@1.21jiggawatts.net>
2017-01-22 12:54:18 +1100

    Make tapping on links in notices open them in Safari

--- Flyweight/Views/ContentView.swift
+++ Flyweight/Views/ContentView.swift
@@ -22,7 +22,57 @@ struct EmbeddedLink {
 }
 
 class ContentView: UILabel {
-    fileprivate var links: [EmbeddedLink] = []
+    private var links: [EmbeddedLink] = []
+    
+    override func awakeFromNib() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapOnLabel))
+        self.isUserInteractionEnabled = true
+        self.addGestureRecognizer(tap)
+        self.lineBreakMode = .byWordWrapping
+        self.numberOfLines = 0
+    }
+    
+    func tapOnLabel(recognizer: UITapGestureRecognizer) {
+        guard let attributedText = self.attributedText else { return }
+        
+        if recognizer.state == .ended {
+            // Set up a text container matching the properties and contents of this label
+            let location = recognizer.location(in: self)
+            let formatted = NSMutableAttributedString(attributedString: attributedText)
+            formatted.addAttributes([NSFontAttributeName: self.font], range: NSMakeRange(0, formatted.string.characters.count))
+            let layoutManager = NSLayoutManager()
+            let textStorage = NSTextStorage(attributedString: formatted)
+            textStorage.addLayoutManager(layoutManager)
+            let textContainer = NSTextContainer(size: self.bounds.size)
+            textContainer.maximumNumberOfLines = self.numberOfLines
+            textContainer.lineBreakMode = self.lineBreakMode
+            textContainer.lineFragmentPadding = 0
+            layoutManager.addTextContainer(textContainer)
+            
+            // Ask what the corresponding character would be at the point we click on
+            // Note that this will get "stuck" on the end of the string for the remainder of the label
+            // To prevent terminating links leaking into empty space we append a zero width space when we set the string
+            let characterIndex = layoutManager.characterIndex(for: location, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+            
+            // If the touch corresponds to any of the contained links, open the URL
+            for l in links {
+                if characterIndex >= l.startIndex && characterIndex < l.endIndex {
+                    // Open the URL in Safari
+                    if let url = URL(string: l.url) {
+                        if UIApplication.shared.canOpenURL(url) {
+                            UIApplication.shared.open(url)
+                        }
+                    }
+                }
+            }
+            
+            // TODO probably need to let the touch fall through somehow when I have tap-for-conversation
+            
+            // Also TODO someday some of these links will be special and do in-app functions instead
+        }
+        
+        
+    }
     
     var htmlContent: String? {
         get {
@@ -30,7 +80,8 @@ class ContentView: UILabel {
         }
         set {
             guard let html = newValue else { return }
-            let (att, links) = ContentView.convertHtmlToAttributedString(html: html)
+            // Add an zero width space to the end because this will allow the URL character matching to terminate
+            let (att, links) = ContentView.convertHtmlToAttributedString(html: html + "\u{200B}")
             self.attributedText = att
             self.links = links
         }
